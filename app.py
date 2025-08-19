bcad57574e41504218b7ba736d0cb48189ae9fe9
Johnnyzhu0 <johnnyzhu048@gmail.com>
2025-08-19 11:26:48 -0400

    add pfp and song display

--- app.py
+++ app.py
@@ -9,11 +9,18 @@ load_dotenv()
 #defining the permissions this app is asking for
 scope = "user-library-read"
 
-
+# Create client with OAuth
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id = os.getenv("SPOTIFY_CLIENT_ID"),
-    client_passcode = os.getenv("SPOTIFY_CLIENT_SECRET"),
-    redirect_url = os.getenv("SPOTIFY_REDIRECT_URL")
+    client_secret = os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri = os.getenv("SPOTIFY_REDIRECT_URL")
 ))
+
+#Test to get current user info
+user = sp.current_user()
+print(f"Logged in as {user['display_name']}")
+print(user['email'])
+print(user['followers'])
+
 
 
